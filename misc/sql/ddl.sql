42a5c3498f10f20f94a5df356a4f081503b3fb86
178inaba <178inaba@users.noreply.github.com>
2017-02-02 12:09:36 +0900

    Add error_log table.

--- misc/sql/ddl.sql
+++ misc/sql/ddl.sql
@@ -5,10 +5,21 @@ USE fizzbuzz_twitterbot;
 DROP TABLE IF EXISTS fizzbuzz;
 CREATE TABLE fizzbuzz (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  tweet_id BIGINT NOT NULL,
   num BIGINT NOT NULL,
+  tweet VARCHAR(255) NOT NULL,
+  tweet_id BIGINT,
+  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at DATETIME NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE InnoDB CHARSET utf8;
+
+DROP TABLE IF EXISTS error_log;
+CREATE TABLE error_log (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  fizzbuzz_id BIGINT NOT NULL,
+  error_message VARCHAR(255) NOT NULL,
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   created_at DATETIME NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (num)
+  FOREIGN KEY (fizzbuzz_id) REFERENCES fizzbuzz (id)
 ) ENGINE InnoDB CHARSET utf8;
