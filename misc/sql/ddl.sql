600f6881c68aac7247cb4302f5daa99c6562426e
178inaba <178inaba@users.noreply.github.com>
2017-02-03 12:39:41 +0900

    Rename table name, fizzbuzz -> fizzbuzz_tweets.

--- misc/sql/ddl.sql
+++ misc/sql/ddl.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS fizzbuzz_twitterbot;
 CREATE DATABASE fizzbuzz_twitterbot CHARACTER SET utf8;
 USE fizzbuzz_twitterbot;
 
-DROP TABLE IF EXISTS fizzbuzz;
-CREATE TABLE fizzbuzz (
+DROP TABLE IF EXISTS fizzbuzz_tweets;
+CREATE TABLE fizzbuzz_tweets (
   id BIGINT NOT NULL AUTO_INCREMENT,
   number BIGINT NOT NULL,
   tweet VARCHAR(140) NOT NULL,
@@ -16,10 +16,10 @@ CREATE TABLE fizzbuzz (
 DROP TABLE IF EXISTS post_errors;
 CREATE TABLE post_errors (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  fizzbuzz_id BIGINT NOT NULL,
+  fizzbuzz_tweet_id BIGINT NOT NULL,
   error_message TEXT NOT NULL,
   updated_at DATETIME NOT NULL,
   created_at DATETIME NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (fizzbuzz_id) REFERENCES fizzbuzz (id)
+  FOREIGN KEY (fizzbuzz_tweet_id) REFERENCES fizzbuzz_tweets (id)
 ) ENGINE InnoDB CHARSET utf8;
