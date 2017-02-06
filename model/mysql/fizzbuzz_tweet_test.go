ce070eeb91635ef2c4b93151a723177b0b9bd1b0
178inaba <178inaba@users.noreply.github.com>
2017-02-06 23:29:06 +0900

    Fix test.

--- model/mysql/fizzbuzz_tweet_test.go
+++ model/mysql/fizzbuzz_tweet_test.go
@@ -1,15 +1,20 @@
 package mysql_test
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
 
 	"github.com/178inaba/fizzbuzz-twitterbot/model"
 	"github.com/178inaba/fizzbuzz-twitterbot/model/mysql"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/stretchr/testify/suite"
 )
 
 type fizzbuzzTweetTestSuite struct {
 	suite.Suite
+
+	db      *sql.DB
 	service mysql.FizzbuzzTweetService
 }
 
@@ -21,6 +26,13 @@ func (s *fizzbuzzTweetTestSuite) SetupTest() {
 	db, err := mysql.Open("root:@/fizzbuzz_twitterbot_test?parseTime=true")
 	s.NoError(err)
 
+	// Reset test db
+	_, err = sq.Delete(model.FizzbuzzTweetTableName).RunWith(db).Exec()
+	s.NoError(err)
+	_, err = db.Exec(fmt.Sprintf("ALTER TABLE %s AUTO_INCREMENT = 1", model.FizzbuzzTweetTableName))
+	s.NoError(err)
+
+	s.db = db
 	s.service = mysql.NewFizzbuzzTweetService(db)
 }
 
@@ -28,5 +40,5 @@ func (s *fizzbuzzTweetTestSuite) TestInsert() {
 	ft := &model.FizzbuzzTweet{Number: 1, Tweet: "test tweet!"}
 	id, err := s.service.Insert(ft)
 	s.NoError(err)
-	s.True(id > 0)
+	s.Equal(uint64(1), id)
 }
