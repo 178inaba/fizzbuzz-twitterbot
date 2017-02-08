40fc9042cda4a3ae8ea2d8a7e77a9299b809db8b
178inaba <178inaba@users.noreply.github.com>
2017-02-08 17:05:10 +0900

    Fix main.

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
+	"github.com/178inaba/fizzbuzz-twitterbot/model/mysql"
+	"github.com/178inaba/fizzbuzz-twitterbot/post"
 	"github.com/ChimeraCoder/anaconda"
 	log "github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
@@ -19,113 +19,53 @@ var (
 	accessTokenSecret = kingpin.Flag("access-token-secret", "Twitter access token secret.").Envar("ACCESS_TOKEN_SECRET").String()
 )
 
-type twitterToken struct {
-	consumerKey       string
-	consumerSecret    string
-	accessToken       string
-	accessTokenSecret string
-}
-
 func main() {
 	kingpin.Parse()
 	os.Exit(run())
 }
 
 func run() int {
-	tt, err := flagValidation()
+	err := flagValidation()
 	if err != nil {
 		log.Errorf("Flag error: %s.", err)
 		return 1
 	}
 
 	// Create twitter client.
-	anaconda.SetConsumerKey(tt.consumerKey)
-	anaconda.SetConsumerSecret(tt.consumerSecret)
-	api := anaconda.NewTwitterApi(tt.accessToken, tt.accessTokenSecret)
+	anaconda.SetConsumerKey(*consumerKey)
+	anaconda.SetConsumerSecret(*consumerSecret)
+	api := anaconda.NewTwitterApi(*accessToken, *accessTokenSecret)
 
-	for i := uint64(1); ; i++ {
-		tweet := tweetText(i)
-		log.Infof("Tweet: %s", tweet)
-
-		// Next post to 00 second.
-		waitNextZeroSec()
-
-		var t anaconda.Tweet
-		for {
-			var err error
-			t, err = api.PostTweet(tweet, nil)
-			if err == nil {
-				break
-			}
-
-			log.Error(err)
-			time.Sleep(time.Second)
-		}
-
-		log.WithField("id", t.Id).Infof("Success!")
+	db, err := mysql.Open("root", "fizzbuzz_twitterbot", true)
+	if err != nil {
+		log.Errorf("DB open error: %s.", err)
+		return 1
 	}
+
+	fts := mysql.NewFizzbuzzTweetService(db)
+	pes := mysql.NewPostErrorService(db)
+	c := post.NewClient(api, fts, pes)
+	c.Post()
+
+	return 0
 }
 
-func nextZeroSec() time.Duration {
-	n := time.Now()
-
-	return n.Truncate(time.Minute).Add(time.Minute).Sub(n)
-}
-
-func waitNextZeroSec() {
-	time.Sleep(nextZeroSec())
-}
-
-func flagValidation() (*twitterToken, error) {
+func flagValidation() error {
 	if *consumerKey == "" {
-		return nil, errors.New("consumer key is not set")
+		return errors.New("consumer key is not set")
 	}
 
 	if *consumerSecret == "" {
-		return nil, errors.New("consumer secret is not set")
+		return errors.New("consumer secret is not set")
 	}
 
 	if *accessToken == "" {
-		return nil, errors.New("access token is not set")
+		return errors.New("access token is not set")
 	}
 
 	if *accessTokenSecret == "" {
-		return nil, errors.New("access token secret is not set")
+		return errors.New("access token secret is not set")
 	}
 
-	return &twitterToken{
-		consumerKey:       *consumerKey,
-		consumerSecret:    *consumerSecret,
-		accessToken:       *accessToken,
-		accessTokenSecret: *accessTokenSecret,
-	}, nil
-}
-
-func tweetText(num uint64) string {
-	tweet, isFB := fizzbuzz(num)
-	if isFB {
-		// Add number hashtag.
-		tweet = fmt.Sprintf("%s #%d", tweet, num)
-	}
-
-	return tweet
-}
-
-func fizzbuzz(num uint64) (string, bool) {
-	var fb string
-	if num%3 == 0 {
-		fb = "Fizz"
-	}
-
-	if num%5 == 0 {
-		fb += "Buzz"
-	}
-
-	isFB := true
-	if len(fb) == 0 {
-		fb = fmt.Sprint(num)
-		isFB = false
-	}
-
-	return fb, isFB
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -1,48 +1,33 @@
 package main
 
 import (
-	"math"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestWaitNextZeroSec(t *testing.T) {
-	next := nextZeroSec()
-	assert.Equal(t, 0, time.Now().Add(next).Second())
-}
 
 func TestFlagValidation(t *testing.T) {
 	strPointer := func(str string) *string {
 		return &str
 	}
 
-	tt, err := flagValidation()
-	assert.Nil(t, tt)
+	err := flagValidation()
 	assert.Error(t, err)
 
 	consumerKey = strPointer("consumer-key")
-	tt, err = flagValidation()
-	assert.Nil(t, tt)
+	err = flagValidation()
 	assert.Error(t, err)
 
 	consumerSecret = strPointer("consumer-secret")
-	tt, err = flagValidation()
-	assert.Nil(t, tt)
+	err = flagValidation()
 	assert.Error(t, err)
 
 	accessToken = strPointer("access-token")
-	tt, err = flagValidation()
-	assert.Nil(t, tt)
+	err = flagValidation()
 	assert.Error(t, err)
 
 	accessTokenSecret = strPointer("access-token-secret")
-	tt, err = flagValidation()
-	assert.Equal(t, *consumerKey, tt.consumerKey)
-	assert.Equal(t, *consumerSecret, tt.consumerSecret)
-	assert.Equal(t, *accessToken, tt.accessToken)
-	assert.Equal(t, *accessTokenSecret, tt.accessTokenSecret)
+	err = flagValidation()
 	assert.NoError(t, err)
 
 	// Tear down.
@@ -50,43 +35,4 @@ func TestFlagValidation(t *testing.T) {
 	consumerSecret = strPointer("")
 	accessToken = strPointer("")
 	accessTokenSecret = strPointer("")
-}
-
-func TestTweetText(t *testing.T) {
-	tweet := tweetText(7)
-	assert.Equal(t, "7", tweet)
-
-	tweet = tweetText(18)
-	assert.Equal(t, "Fizz #18", tweet)
-
-	tweet = tweetText(20)
-	assert.Equal(t, "Buzz #20", tweet)
-
-	tweet = tweetText(45)
-	assert.Equal(t, "FizzBuzz #45", tweet)
-
-	tweet = tweetText(math.MaxUint64)
-	assert.Equal(t, "FizzBuzz #18446744073709551615", tweet)
-}
-
-func TestFizzBuzz(t *testing.T) {
-	fb, isFB := fizzbuzz(7)
-	assert.Equal(t, "7", fb)
-	assert.False(t, isFB)
-
-	fb, isFB = fizzbuzz(18)
-	assert.Equal(t, "Fizz", fb)
-	assert.True(t, isFB)
-
-	fb, isFB = fizzbuzz(20)
-	assert.Equal(t, "Buzz", fb)
-	assert.True(t, isFB)
-
-	fb, isFB = fizzbuzz(45)
-	assert.Equal(t, "FizzBuzz", fb)
-	assert.True(t, isFB)
-
-	fb, isFB = fizzbuzz(math.MaxUint64)
-	assert.Equal(t, "FizzBuzz", fb)
-	assert.True(t, isFB)
 }
