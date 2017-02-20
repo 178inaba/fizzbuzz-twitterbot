41143a5464b833908f954b80ab2373c72d645898
178inaba <178inaba@users.noreply.github.com>
2017-02-21 02:14:46 +0900

    Fix test using suite.

--- model/mysql/mysql_test.go
+++ model/mysql/mysql_test.go
@@ -1,28 +1,60 @@
 package mysql
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
 
 	"github.com/178inaba/fizzbuzz-twitterbot/model"
 	sq "github.com/Masterminds/squirrel"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestPrepareExecer(t *testing.T) {
+type prepareExecerSuite struct {
+	suite.Suite
+
+	db     *sql.DB
+	execer prepareExecer
+}
+
+func TestPrepareExecerSuite(t *testing.T) {
+	suite.Run(t, new(prepareExecerSuite))
+}
+
+func (s *prepareExecerSuite) SetupSuite() {
 	db, err := Open("root", "", "fizzbuzz_twitterbot_test")
-	assert.NoError(t, err)
+	s.NoError(err)
 
-	pe := prepareExecer{db: db}
+	s.db = db
+	s.execer = prepareExecer{db: db}
+}
 
-	res, err := pe.Exec("prepare error")
-	assert.Error(t, err)
-	assert.Nil(t, res)
+func (s *prepareExecerSuite) SetupTest() {
+	// Reset test db.
+	_, err := s.db.Exec("SET FOREIGN_KEY_CHECKS = 0")
+	s.NoError(err)
+	_, err = s.db.Exec(fmt.Sprintf("TRUNCATE TABLE %s", model.PostErrorTableName))
+	s.NoError(err)
+	_, err = s.db.Exec(fmt.Sprintf("TRUNCATE TABLE %s", model.FizzbuzzTweetTableName))
+	s.NoError(err)
+	_, err = s.db.Exec("SET FOREIGN_KEY_CHECKS = 1")
+	s.NoError(err)
+}
+
+func (s *prepareExecerSuite) TestInsert() {
+	res, err := s.execer.Exec("prepare error")
+	s.Error(err)
+	s.Nil(res)
 
 	sql, _, err := sq.Insert(model.FizzbuzzTweetTableName).
 		Columns("number").Values(struct{}{}).ToSql()
-	assert.NoError(t, err)
+	s.NoError(err)
 
-	res, err = pe.Exec(sql)
-	assert.Error(t, err)
-	assert.Nil(t, res)
+	res, err = s.execer.Exec(sql)
+	s.Error(err)
+	s.Nil(res)
+}
+
+func (s *prepareExecerSuite) TearDownSuite() {
+	s.db.Close()
 }
