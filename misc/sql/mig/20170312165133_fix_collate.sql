3b88a2b07ff0a2ee6c9462865f085c98beab5204
178inaba <178inaba@users.noreply.github.com>
2017-03-13 01:53:32 +0900

    Add migration sql.

---
+++ misc/sql/mig/20170312165133_fix_collate.sql
@@ -0,0 +1,3 @@
+ALTER DATABASE fizzbuzz_twitterbot COLLATE utf8mb4_bin;
+ALTER TABLE fizzbuzz_tweets CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+ALTER TABLE post_errors CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
